688d88b9f1abf5c8695f3da0e2b597582c452b3a
knyaka <kn9kaa@gmail.com>
2018-04-28 02:20:46 +0300

    store data in json && get building costs

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 import puppeteer from 'puppeteer'
 import { config } from './config.js'
+import jsonfile from 'jsonfile'
+import fs from 'fs'
 
-const some = () => 'hi'
+let debug = true
+const server = config.serverUrl
 const run = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
 
-  await page.goto('https://ts4.travian.ru')
+  // create folders 
+  const accountFolder = `./storage/${config.login}`
+  if (!fs.existsSync(accountFolder)) {
+    fs.mkdirSync('./storage')
+    fs.mkdirSync(accountFolder)
+  }
+
+  // init browser
+  const browser = await puppeteer.launch({ headless: !debug })
+  const page = await browser.newPage()
+  const jsonCallback = (e) => !e ? null : console.log('\x1b[31msomething goes wrong: ', e) // callback for json write and read
+
+  await page.goto(server)
 
   // login
   const LOGIN_SELECTOR = 'td input[name="name"]'
@@ -23,7 +36,7 @@ const run = async () => {
   await page.waitForNavigation({ waitUntil: 'networkidle2' })
 
   // production 
-  const production = await page.evaluate(() => {
+  const productionRate = await page.evaluate(() => {
     const table = document.getElementById('production').querySelectorAll('td[class="num"]')
     return {
       wood: table[0].textContent.trim(),
@@ -32,7 +45,7 @@ const run = async () => {
       crop: table[3].textContent.trim()
     }
   })
-  console.log('production: ', production)
+  jsonfile.writeFile(`${accountFolder}/production-rate.json`, productionRate, jsonCallback)
 
   // resources
   const resourceStock = await page.evaluate(() => {
@@ -43,23 +56,24 @@ const run = async () => {
       crop: document.getElementById('l4').textContent.trim()
     }
   })
-  console.log('current stock: ', resourceStock)
+  jsonfile.writeFile(`${accountFolder}/resource-stock.json`, resourceStock, jsonCallback)
 
   // max stock 
   const maxStock = await page.evaluate(() => {
     const maxResourceStock = document.getElementById('stockBarWarehouse').textContent.trim()
     const maxCropStock = document.getElementById('stockBarGranary').textContent.trim()
+    const freeCropStock = document.getElementById('stockBarFreeCrop').textContent.trim()
     return {
       warehouse: maxResourceStock,
-      crop: maxCropStock
+      crop: maxCropStock,
+      freeCrop: freeCropStock
     }
   })
-  console.log('max stock: ', maxStock)
+  jsonfile.writeFile(`${accountFolder}/max-stock.json`, maxStock, jsonCallback)
 
   // building
   // другая реализация let buildings = await page.$$('div.level')
-
-  const buildings = await page.evaluate(() => {
+  const resourceFields = await page.evaluate(() => {
     const b = []
     const types = { gid1: 'wood', gid2: 'clay', gid3: 'iron', gid4: 'crop' }
     const buildings = document.querySelectorAll('div.level')
@@ -68,7 +82,7 @@ const run = async () => {
         let fieldInfo = buildings[field].className.split(' ')
         // TODO: переписать на отдельную функцию с поиском индекса, т.к. сейчас на 3 индексе может быть флаг постройки
         b.push({
-          id: field,
+          id: Number(field) + 1,
           underConstruction: false,
           type: types[fieldInfo[3]],
           level: fieldInfo[4].replace('level', '')
@@ -77,15 +91,32 @@ const run = async () => {
     }
     return b
   })
+  jsonfile.writeFile(`${accountFolder}/resource-fields.json`, resourceFields, jsonCallback)
 
-  console.log(buildings)
+  // get build costs for resource fields
+  let resourceFieldsBuildingCosts = []
+  for (let field in resourceFields) {
+    let id = resourceFields[field].id
+    await page.goto(`${server}/build.php?id=${id}`)
+    await page.waitForSelector('span.resources.r4') // wait until the last html tag of build costs is not loaded 
+    let buildingCost = await page.evaluate(() => {
+      let wood = document.querySelector('span.resources.r1').textContent.trim()
+      let clay = document.querySelector('span.resources.r2').textContent.trim()
+      let iron = document.querySelector('span.resources.r3').textContent.trim()
+      let crop = document.querySelector('span.resources.r4').textContent.trim()
+      return { wood, clay, iron, crop }
+    })
+    resourceFieldsBuildingCosts.push({ id: id, ...buildingCost })
+  }
+  jsonfile.writeFile(`${accountFolder}/build-cost.json`, resourceFieldsBuildingCosts, jsonCallback)
 
   /* TODO: 
-    получение списка полей с типом, расположением и уровнем
-    создание хранилища для стоимости постройки
-    создание функций для навигации и нажатия на постройку
+    функцию постройки (не забыть про uuid который вешается на клик по кнопке строить, его нужно подставлять в юрл, если этого не происходит)
+    чтение квестов -  скриптинг для квестов по оптимальному развитию
+    начать разбирать героя, прокачку и отправку на приключения
     https://github.com/emadehsan/thal
   */
+
 }
 
 run()
